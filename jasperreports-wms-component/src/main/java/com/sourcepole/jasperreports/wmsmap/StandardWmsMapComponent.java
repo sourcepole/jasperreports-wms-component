e1a4baddc2c58695dc1ea928a414bc6c784328a6
tam <tam@panter.ch>
2013-07-16 23:12:20 +0200

    Remove original license header

--- jasperreports-wms-component/src/main/java/com/sourcepole/jasperreports/wmsmap/StandardWmsMapComponent.java
+++ jasperreports-wms-component/src/main/java/com/sourcepole/jasperreports/wmsmap/StandardWmsMapComponent.java
@@ -1,26 +1,3 @@
-/*
- * JasperReports - Free Java Reporting Library.
- * Copyright (C) 2001 - 2011 Jaspersoft Corporation. All rights reserved.
- * http://www.jaspersoft.com
- *
- * Unless you have purchased a commercial license agreement from Jaspersoft,
- * the following license terms apply:
- *
- * This program is part of JasperReports.
- *
- * JasperReports is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * JasperReports is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
- */
 package com.sourcepole.jasperreports.wmsmap;
 
 import java.io.Serializable;
@@ -239,7 +216,7 @@ public class StandardWmsMapComponent implements WmsMapComponent, Serializable,
     clone.layersExpression = JRCloneUtils.nullSafeClone(layersExpression);
     clone.stylesExpression = JRCloneUtils.nullSafeClone(stylesExpression);
     clone.urlParametersExpression = JRCloneUtils
-            .nullSafeClone(urlParametersExpression);
+        .nullSafeClone(urlParametersExpression);
     clone.eventSupport = null;
     return clone;
   }
