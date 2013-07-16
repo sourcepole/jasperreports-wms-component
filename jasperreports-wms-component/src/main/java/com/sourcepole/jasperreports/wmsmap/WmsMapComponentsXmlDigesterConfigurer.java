0faee7e45b2b7b661140ff34d3c3f66e60d2d108
tam <tam@panter.ch>
2013-07-16 23:13:34 +0200

    Remove inappropriate javadoc

--- jasperreports-wms-component/src/main/java/com/sourcepole/jasperreports/wmsmap/WmsMapComponentsXmlDigesterConfigurer.java
+++ jasperreports-wms-component/src/main/java/com/sourcepole/jasperreports/wmsmap/WmsMapComponentsXmlDigesterConfigurer.java
@@ -6,13 +6,6 @@ import net.sf.jasperreports.engine.xml.JRExpressionFactory;
 
 import org.apache.commons.digester.Digester;
 
-/**
- * XML digester for built-in component implementations.
- * 
- * @author Lucian Chirita (lucianc@users.sourceforge.net)
- * @version $Id: ComponentsXmlHandler.java 5655 2012-09-12 13:25:21Z teodord $
- * @see ComponentsExtensionsRegistryFactory
- */
 public class WmsMapComponentsXmlDigesterConfigurer implements
     XmlDigesterConfigurer
 {
