894025e00f3a92222063d5138d622f8cfcc4bdfe
tam <tam@panter.ch>
2013-07-17 23:49:30 +0200

    Add license header to all java source files

--- ireport-wms-plugin/src/com/sourcepole/ireport/components/wmsmap/WmsMapComponentWidget.java
+++ ireport-wms-plugin/src/com/sourcepole/ireport/components/wmsmap/WmsMapComponentWidget.java
@@ -1,3 +1,22 @@
+/*
+ * JasperReports/iReport WMS Component
+ * 
+ * Copyright (C) 2013 Sourcepole AG
+ * 
+ * JasperReports/iReport WMS Component is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * JasperReports/iReport WMS Component is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JasperReports/iReport WMS Component.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.sourcepole.ireport.components.wmsmap;
 
 import com.jaspersoft.ireport.designer.AbstractReportObjectScene;
